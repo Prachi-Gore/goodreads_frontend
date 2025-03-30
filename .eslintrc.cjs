b288d35efd3821f82ef801eddf03f63f7604401a
Prachi Gore <prachigore408@gmail.com>
2025-03-30 21:21:34 +0530

    eslint rules removed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
-    // "simple-import-sort/imports": "error",
+    // "simple-import-sort/imports": "error", // sorting of import statements
     // "semi": [1, "always"],
-    "react/prop-types": "off",
+    "react/prop-types": "off", // PropTypes validation is turned off.
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
